7dbce9ae9dab471f6ba0e2b1a5e5a3f7593036cd
Peca Nesovanovic <Npeca75@users.noreply.github.com>
2021-10-15 11:34:02 +0000

    Apply fixes from StyleCI

--- routeros.inc.php
+++ routeros.inc.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * routeros.inc.php
+ * routeros.inc.php.
  *
  * LibreNMS vlan discovery module for Mikrotik RouterOS
  *
@@ -33,62 +33,62 @@ $oids = snmp_walk($device, '.1.3.6.1.4.1.14988.1.1.8.1.1.2', '-Osqn', '');
 $oids = trim($oids);
 
 if ($oids) {
-    echo 'Mikrotik VLANs ' . "\n";
+    echo 'Mikrotik VLANs '."\n";
 
     foreach (explode("\n", $oids) as $data) {
-	if ($data) {
-	    $split = trim(explode(' ', $data)[0]);
-	    $value = trim(explode(' ', $data)[1]);
-	    $si = explode('.', $split)[14]; //Script Index
-	    if ($value == "LNMS_vlans") {
-		$sIndex=$si;
-	    }
-	}
+        if ($data) {
+            $split = trim(explode(' ', $data)[0]);
+            $value = trim(explode(' ', $data)[1]);
+            $si = explode('.', $split)[14]; //Script Index
+            if ($value == 'LNMS_vlans') {
+                $sIndex = $si;
+            }
+        }
     }
 }
 
-if ( isset($sIndex) ) {
-    d_echo ("Mikrotik script found");
+if (isset($sIndex)) {
+    d_echo('Mikrotik script found');
     $vlanversion = 1;
-    $data = snmp_get($device, '.1.3.6.1.4.1.14988.1.1.18.1.1.2.' . $sIndex, '-Ovq' , '');
+    $data = snmp_get($device, '.1.3.6.1.4.1.14988.1.1.18.1.1.2.'.$sIndex, '-Ovq', '');
     $data = trim($data);
     $oldId = 0;
     foreach (preg_split("/((\r?\n)|(\r\n?))/", $data) as $line) {
-	$vType = trim(explode(',', $line)[0]);
-	$vId = trim(explode(',', $line)[1]);
-	$vIf = trim(explode(',', $line)[2]);
-	$vName = 'Vlan_' . $vId;
-	if ( $oldId != $vId ) {
-	    $oldId = $vId;
-	    $device['vlans'][1][$vId] = $vId;
+        $vType = trim(explode(',', $line)[0]);
+        $vId = trim(explode(',', $line)[1]);
+        $vIf = trim(explode(',', $line)[2]);
+        $vName = 'Vlan_'.$vId;
+        if ($oldId != $vId) {
+            $oldId = $vId;
+            $device['vlans'][1][$vId] = $vId;
 
-	    $old_data = dbFetchRow('select * FROM vlans where `device_id` = ? AND `vlan_vlan` = ? AND `vlan_domain` = ?', [$device['device_id'], $vId, 1]);
-	    if (isset($old_data)) {
-		if ($old_data['vlan_name'] != $vName) {
-		    $vlan_upd['vlan_name'] = $vName;
-		    dbUpdate($vlan_upd, 'vlans', '`vlan_id` = ?', [$vId]);
-		    echo 'U';
-		} else {
-		    echo '.';
-		}
-	    } else {
-		dbInsert([
-		    'device_id' => $device['device_id'],
-		    'vlan_domain' => 1,
-		    'vlan_vlan' => $vId,
-		    'vlan_name' => $vName,
-		    'vlan_type' => ['NULL'],
-		    ], 'vlans');
-		echo '+';
-	    }
-	}
-	$port = dbFetchRow('select * FROM ports WHERE `device_id` = ? AND `ifName` = ?', [$device['device_id'], $vIf]);
-	$ifIndex = $port['ifIndex'];
-	d_echo ("\n ifIndex from DB: $ifIndex \n");
-	if ($vType == "U") {
-	    $per_vlan_data[$vId][$ifIndex]['untagged'] = 1;
-	} else {
-	    $per_vlan_data[$vId][$ifIndex]['untagged'] = 0;
-	}
+            $old_data = dbFetchRow('select * FROM vlans where `device_id` = ? AND `vlan_vlan` = ? AND `vlan_domain` = ?', [$device['device_id'], $vId, 1]);
+            if (isset($old_data)) {
+                if ($old_data['vlan_name'] != $vName) {
+                    $vlan_upd['vlan_name'] = $vName;
+                    dbUpdate($vlan_upd, 'vlans', '`vlan_id` = ?', [$vId]);
+                    echo 'U';
+                } else {
+                    echo '.';
+                }
+            } else {
+                dbInsert([
+                    'device_id'   => $device['device_id'],
+                    'vlan_domain' => 1,
+                    'vlan_vlan'   => $vId,
+                    'vlan_name'   => $vName,
+                    'vlan_type'   => ['NULL'],
+                ], 'vlans');
+                echo '+';
+            }
+        }
+        $port = dbFetchRow('select * FROM ports WHERE `device_id` = ? AND `ifName` = ?', [$device['device_id'], $vIf]);
+        $ifIndex = $port['ifIndex'];
+        d_echo("\n ifIndex from DB: $ifIndex \n");
+        if ($vType == 'U') {
+            $per_vlan_data[$vId][$ifIndex]['untagged'] = 1;
+        } else {
+            $per_vlan_data[$vId][$ifIndex]['untagged'] = 0;
+        }
     }
 }
